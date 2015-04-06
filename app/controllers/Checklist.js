f30ebf766c4d52b74f3c8d6d8485e11f574b8be8
unknown <uh968129@CC205_08.wtstudents.wtamu.edu>
2015-04-06 08:51:28 -0500

    Working on checkboxes

--- app/controllers/Checklist.js
+++ app/controllers/Checklist.js
@@ -1,1 +1,20 @@
 var args = arguments[0] || {};
+
+
+var typeCheckBox = Titanium.UI.createButton({
+        image: '../images/unchecked.png',
+    checked:false,
+        width: 20,
+        height: 20,
+        left: 10
+    });
+ 
+    typeCheckBox.addEventListener('click', function(e){
+        if (e.source.checked == true){
+            e.source.image = '../images/unchecked.png',
+            e.source.checked = false;
+        } else {
+            e.source.image = '../images/checked.png',
+            e.source.checked = true;
+        };
+    });
